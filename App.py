626fbb1c3980134b76bde6adf505d1547b88531a
zapluc75 <lucianopaz@yahoo.com.br>
2025-11-16 21:41:37 -0300

    Update App.py

--- App.py
+++ App.py
@@ -19,45 +19,50 @@ if arquivo:
         st.error(f"Erro ao carregar o arquivo: {e}")
         st.stop()
 
-    st.subheader("📄 Tabela Completa")
-    fig_tabela = go.Figure(
-        data=[
-            go.Table(
-                header=dict(
-                    values=list(df.columns),
-                    fill_color="lightgreen",
-                    align="center",font=dict(color="black", size=12)
-                ),
-                cells=dict(
-                    values=[df[c].tolist() for c in df.columns],
-                    fill_color="gray",
-                    align="center",font=dict(color="white", size=14)
+    col1, col2, col3 = st.columns([1, 2, 1]) # Proporção de largura: 1/4, 2/4 (metade), 1/4
+
+    with col2:
+        st.subheader("📄 Tabela Completa")
+        fig_tabela = go.Figure(
+            data=[
+                go.Table(
+                    header=dict(
+                        values=list(df.columns),
+                        fill_color="lightgreen",
+                        align="center",font=dict(color="black", size=12)
+                    ),
+                    cells=dict(
+                        values=[df[c].tolist() for c in df.columns],
+                        fill_color="gray",
+                        align="center",font=dict(color="white", size=14)
+                    )
                 )
-            )
-        ]
-    )
+            ]
+        )
 
     st.plotly_chart(fig_tabela, use_container_width=True)
     
     # --- Configuração do Gráfico ---
-    st.subheader("📊 Gráfico de Barras")
+    cl1, cl2, cl3 = st.columns([1, 2, 1]) # Proporção de largura: 1/4, 2/4 (metade), 1/4
 
-    col_nome_x = st.selectbox("Coluna para eixo X", df.columns)
-    col_nome_y = st.selectbox("Coluna para eixo Y", df.columns)
-
-    fig_barra = px.bar(
-        df,
-        x=col_nome_x,
-        y=col_nome_y,
-        title=f"Gráfico de Barras de {col_nome_y} por {col_nome_x}",
-        labels={col_nome_x: "Categoria", col_nome_y: "Valor"},
-        hover_data={col_nome_x: True, col_nome_y: True},
-        color=col_nome_x,
-        text_auto=True
-    )
-
-    st.plotly_chart(fig_barra, use_container_width=True)
+    with cl2:
+        st.subheader("📊 Gráfico de Barras")
+    
+        col_nome_x = st.selectbox("Coluna para eixo X", df.columns)
+        col_nome_y = st.selectbox("Coluna para eixo Y", df.columns)
+    
+        fig_barra = px.bar(
+            df,
+            x=col_nome_x,
+            y=col_nome_y,
+            title=f"Gráfico de Barras de {col_nome_y} por {col_nome_x}",
+            labels={col_nome_x: "Categoria", col_nome_y: "Valor"},
+            hover_data={col_nome_x: True, col_nome_y: True},
+            color=col_nome_x,
+            text_auto=True
+        )
+    
+        st.plotly_chart(fig_barra, use_container_width=True)
   
-
 else:
     st.info("Envie um arquivo Excel na barra lateral para começar.")
